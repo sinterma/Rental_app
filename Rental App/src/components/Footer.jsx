04c7f18f8a4870e3df802b286c0fb7248d92d1e8
Sosha Fedotovskaia <sinterma@gmail.com>
2025-03-11 17:44:35 +0100

    first commit

---
+++ Rental App/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        Check the project on <a href="https://github.com/sinterma/Apartment_app">GitHub</a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Rental App/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <img src="logo.png" alt="App Logo" className="logo" />
+      <h1 className="app-name">Smart Rental</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
